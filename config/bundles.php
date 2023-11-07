1d876e14b032cd4fd71a71aa67531ca31a831fc9
MaianaMiramon <maiana.miramon@gmail.com>
2023-11-07 16:31:35 +0100

    Fix: remove relmio-api-doc - repair CORS in progress

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
